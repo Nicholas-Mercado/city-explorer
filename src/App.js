9a1bc9194b2e1eb1cc1fa9fbcad4d30a07996690
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-21 18:17:21 -0800

    Completed map rendering

--- src/App.js
+++ src/App.js
@@ -20,14 +20,22 @@ class App extends React.Component {
   
   getCityData = async (e) => {
     e.preventDefault();
+    
+
     let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+    
     this.setState({
       cityData: cityData.data[0]
     })
     console.log(cityData.data[0]);
   }
+
+  
 render(){
   // console.log(process.env.REACT_APP_LOCATIONIQ_API_KEY)
+  let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10`;
+  
+
   return(
     <>
       <h1> City Explorer 4</h1>
@@ -38,7 +46,13 @@ render(){
           <button type="submit">Explore!</button>
       </form>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="" />
+      {
+        this.state.cityData.lat 
+        ?
+        <Card.Img variant="top" src= {cityMap} />
+        :
+        <Card.Img/>
+      }
           <Card.Body>
           <Card.Title>{this.state.cityData.display_name}</Card.Title>
           <Card.Text>{this.state.cityData.lon}</Card.Text>

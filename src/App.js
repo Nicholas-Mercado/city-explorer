949cda0423c4669b60bbdb2c5298f125e19b625d
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-21 20:41:11 -0800

    created error handling

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ class App extends React.Component {
     super(props);
     this.state = {
       city: '',
-      cityData: {}
+      cityData: {},
+      error: false,
+      errorMessage: ''
     }
   }
-
+  
+  
   handleCityInput = (e) => {
     e.preventDefault();
     this.setState({
@@ -24,21 +27,27 @@ class App extends React.Component {
   getCityData = async (e) => {
     e.preventDefault();
     
-
-    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+  try {
+    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_AI_KEY}&q=${this.state.city}&format=json`);
     
     this.setState({
-      cityData: cityData.data[0]
-    })
-    console.log(cityData.data[0]);
+      cityData: cityData.data[0] })
+    } catch (error) {
+      console.log(error);
+      
+      this.setState({
+        error:true,
+        errorMessage: `You have a Error: ${error.response.status}` 
+      })
+
+    }
   }
 
-  
+
 render(){
   // console.log(process.env.REACT_APP_LOCATIONIQ_API_KEY)
   let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=14`;
   
-
   return(
     <>
       <h1> City Explorer 4</h1>
@@ -48,20 +57,27 @@ render(){
         </Form.Label>
           <Button type="submit">Explore!</Button>
       </Form>
-      <Card style={{ width: '36rem' }}>
       {
-        this.state.cityData.lat 
-        ?
-        <Card.Img variant="top" src= {cityMap} />
+        this.state.error 
+        ? 
+        
+        <p>{this.state.errorMessage}</p>
         :
-        <Card.Img/>
-      }
+       <Card style={{ width: '36rem' }}>
+          {
+            this.state.cityData.lat 
+            ?
+            <Card.Img variant="top" src= {cityMap} />
+            :
+            <Card.Img/>
+          }
           <Card.Body>
-          <Card.Title>{this.state.cityData.display_name}</Card.Title>
-          <Card.Text>{this.state.cityData.lon}</Card.Text>
-          <Card.Text>{this.state.cityData.lat}</Card.Text>
+            <Card.Title>{this.state.cityData.display_name}</Card.Title>
+            <Card.Text>{this.state.cityData.lon}</Card.Text>
+            <Card.Text>{this.state.cityData.lat}</Card.Text>
         </Card.Body>
       </Card>
+      }
     </>
   )
 }

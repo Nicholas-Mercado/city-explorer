9dc62464aab6602a84c6823bfa82398a2a3368d8
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-22 20:32:54 -0800

    completed Weather (placeholer)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Card from 'react-bootstrap/Card';
 import "./App.css"
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Weather from './Weather';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 class App extends React.Component {
   constructor(props){
@@ -12,10 +15,11 @@ class App extends React.Component {
       city: '',
       cityData: {},
       error: false,
-      errorMessage: ''
+      errorMessage: '',
+      cityWeather:[],
     }
+    // console.log(this.state);
   }
-  
   
   handleCityInput = (e) => {
     e.preventDefault();
@@ -29,9 +33,13 @@ class App extends React.Component {
     
   try {
     let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    
+                                       //http://localhost:3001/weather?searchQuery=Paris
+    let cityWeather = await axios.get(`http://localhost:3001/weather?searchQuery=${this.state.city}`);
+    console.log(cityWeather);
     this.setState({
-      cityData: cityData.data[0] })
+      cityWeather: cityWeather.data,
+      cityData: cityData.data[0] 
+    })
     } catch (error) {
       // console.log(error);
       
@@ -45,7 +53,7 @@ class App extends React.Component {
 
 
 render(){
-  // console.log(process.env.REACT_APP_LOCATIONIQ_API_KEY)
+  console.log(this.state.cityWeather);
   let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=14`;
   
   return(
@@ -78,6 +86,16 @@ render(){
         </Card.Body>
       </Card>
       }
+      <Row xs={1} sm={2} md={3} lg={3} className="mt-5">
+              {this.state.cityWeather.map((day, index) => (
+                <Col key={index}>
+                  <Weather 
+                  cityWeather={day}
+                    city={this.state.city}
+                    />
+                </Col>
+              ))}
+            </Row> 
     </>
   )
 }
---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+
+
+class Weather extends React.Component{
+render(){
+  // console.log(this.props.cityWeather);
+  return(
+    <>
+      
+      <Card>
+        <Card.Body>
+          <Card.Title> {this.props.city}</Card.Title>
+          <Card.Text>{this.props.cityWeather.date}</Card.Text>
+          <Card.Text>{this.props.cityWeather.description}</Card.Text>
+        </Card.Body>    
+      </Card>
+    </>
+  )
+};
+}
+export default Weather;
